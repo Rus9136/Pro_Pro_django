b89464f31a9b267dc3927bdb702073e40a86f0c7
Daurenov <3350698@mail.ru>
2022-08-16 19:43:24 +0600

    init

---
+++ coolsite/women/tests.py
@@ -0,0 +1,13 @@
+a = [55, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+result = list(filter(lambda elem: elem in b, a))
+ss = lambda x, y: x * y
+rr = list(map(lambda x: x**2, a))
+
+
+
+
+a.sort()
+print(a)
