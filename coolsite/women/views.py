a34e99b2317fb837dff8d552ae6be8253998643c
Daurenov <3350698@mail.ru>
2022-08-16 21:23:49 +0600

    Добавил модель women

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Страница приложение django.")
+
+
+def categories(request):
+    if request.GET:
+        print(request.get)
+    return HttpResponse("Страница приложение categories.")
